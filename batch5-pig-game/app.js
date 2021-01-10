b1033fe98dc555b10b1af9f5a98e64e22cdbc7af
Harry Bolingot <harrybolingot@gmail.com>
2021-01-10 21:37:17 +0800

    Minor update

--- batch5-pig-game/app.js
+++ batch5-pig-game/app.js
@@ -88,32 +88,3 @@ function gameOver() {
 }
 
 startNewGame();
-
-var modal = document.getElementById("myModal");
-var modalContent = document.getElementById("myModal-content");
-
-var btn = document.getElementById("myBtn");
-var closeButton = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    if (modal.classList.contains('show')){
-        modal.classList.remove('show');
-        modalContent.classList.remove('show');
-    } 
-    else{
-        modal.classList.add('show');
-        modalContent.classList.add('show');
-    } 
-}
-
-closeButton.onclick = function() {
-    modal.classList.remove('show');
-    modalContent.classList.remove('show');
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.classList.remove('show');
-    modalContent.classList.remove('show');
-  }
-}
